c3f8712140b84542eeec79b5a039bfdc3c327e68
dan <me@danrh.co.uk>
2023-05-16 11:24:36 -0400

    feat: add demo page for github pages

--- config.js
+++ config.js
@@ -1,4 +1,14 @@
-const config = {
+// Default config
+var config = {
     "base_url": "http://localhost:7060/comments",
     "page_id": "add-page-id-here"
+}
+
+
+// github pages config
+if (window.location.href === 'https://danielrholland.github.io/simple-comments-widget/client.html'){
+  config = {
+    base_url : 'https://comments.dotemgo.com/comments',
+    page_id : 'https://danielrholland.github.io/simple-comments-widget/client.html'
+  };
 }
