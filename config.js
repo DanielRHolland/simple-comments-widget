0ad1eb1af12035d2aee5daf816fa59d0eca6fd48
dan <me@danrh.co.uk>
2023-05-16 11:45:45 -0400

    Revert "debug: add console.log(window.location.href) after config init"
    This reverts commit 570e541bf5eda2aea669ce5b2503159819f3b90e.

--- config.js
+++ config.js
@@ -12,4 +12,3 @@ if (window.location.href === 'https://danielrholland.github.io/simple-comments-w
     page_id : 'https://danielrholland.github.io/simple-comments-widget/client.html'
   };
 }
-console.log(window.location.href)
